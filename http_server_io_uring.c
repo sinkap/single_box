15a0e93215d561aaabb0a778e7cb0c00d341a492
KP Singh <kpsingh@kernel.org>
2024-10-30 02:55:04 +0100

    fix an fd leak and data corruption

--- http_server_io_uring.c
+++ http_server_io_uring.c
@@ -16,197 +16,179 @@
 #define NUM_PENDING_CONNECTIONS 32
 
 const char *hello_response = "HTTP/1.1 200 OK\r\n"
-			     "Content-Type: text/html\r\n"
-			     "Content-Length: 13\r\n"
-			     "\r\n"
-			     "<h1>Hello</h1>";
+                             "Content-Type: text/html\r\n"
+                             "Content-Length: 13\r\n"
+                             "\r\n"
+                             "<h1>Hello</h1>";
 
 enum op_type {
-	OP_ACCEPT,
-	OP_READ,
-	OP_WRITE,
+    OP_ACCEPT,
+    OP_READ,
+    OP_WRITE,
 };
 
 struct client_data {
-	struct sockaddr_in client_addr;
-	socklen_t client_addr_len;
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len;
 };
 
-// To be set as user_data for accept requests.
 struct accept_data {
-	enum op_type type;
-	int server_fd;
+    enum op_type type;
+    int server_fd;
 };
 
 struct read_data {
-	enum op_type type;
-	int fd;
-	char buffer[BUFFER_SIZE];
+    enum op_type type;
+    int fd;
+    char buffer[BUFFER_SIZE];
 };
 
 struct write_data {
-	enum op_type type;
-	int fd;
+    enum op_type type;
+    int fd;
 };
 
 int setup_server_socket_or_die(int port)
 {
-	struct sockaddr_in server_addr;
-	int server_fd;
+    struct sockaddr_in server_addr;
+    int server_fd;
 
-	/* Create a TCP server socket */
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd == -1) {
-		perror("socket");
-		exit(-1);
-	}
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        perror("socket");
+        exit(-1);
+    }
 
-	int optval = 1;
-	/* SO_REUSEADDR to quickly restart the server */
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &optval,
-		       sizeof(optval)) == -1) {
-		perror("setsockopt");
-		exit(-1);
-	}
+    int optval = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) == -1) {
+        perror("setsockopt");
+        exit(-1);
+    }
 
-	memset(&server_addr, 0, sizeof(server_addr));
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = INADDR_ANY;
-	server_addr.sin_port = htons(port);
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
 
-	if (bind(server_fd, (struct sockaddr *)&server_addr,
-		 sizeof(server_addr)) == -1) {
-		perror("bind");
-		exit(-1);
-	}
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("bind");
+        exit(-1);
+    }
 
-	if (listen(server_fd, NUM_PENDING_CONNECTIONS) == -1) {
-		perror("listen");
-		exit(-1);
-	}
-	return server_fd;
+    if (listen(server_fd, NUM_PENDING_CONNECTIONS) == -1) {
+        perror("listen");
+        exit(-1);
+    }
+    return server_fd;
 }
 
-struct client_data *alloc_client_data_or_die()
+void add_accept_request(struct io_uring *ring, int server_fd)
 {
-	struct client_data *data =
-		(struct client_data *)malloc(sizeof(struct client_data));
-	if (!data) {
-		perror("malloc");
-		exit(1);
-	}
+    struct accept_data *accept_data = malloc(sizeof(struct accept_data));
+    accept_data->type = OP_ACCEPT;
+    accept_data->server_fd = server_fd;
 
-	return data;
+    struct client_data *client_data = malloc(sizeof(struct client_data));
+    client_data->client_addr_len = sizeof(client_data->client_addr);
+
+    struct io_uring_sqe *sqe = io_uring_get_sqe(ring);
+    io_uring_prep_accept(sqe, server_fd, (struct sockaddr *)&client_data->client_addr,
+                         &client_data->client_addr_len, 0);
+    io_uring_sqe_set_data(sqe, accept_data);
 }
 
-void add_accept_request(struct io_uring *ring, struct client_data *client_data,
-			struct accept_data *accept_data)
+void add_write_request(struct io_uring *ring, int client_fd)
 {
-	struct io_uring_sqe *sqe = io_uring_get_sqe(ring);
-	io_uring_prep_accept(sqe, accept_data->server_fd,
-			     (struct sockaddr *)&client_data->client_addr,
-			     &client_data->client_addr_len, 0);
-	io_uring_sqe_set_data(sqe, accept_data);
-}
+    struct write_data *wd = malloc(sizeof(struct write_data));
+    wd->type = OP_WRITE;
+    wd->fd = client_fd;
 
-void add_write_request(struct io_uring *ring, struct write_data *wd)
-{
-	struct io_uring_sqe *sqe = io_uring_get_sqe(ring);
-	io_uring_prep_write(sqe, wd->fd, hello_response, strlen(hello_response),
-			    0);
-	io_uring_sqe_set_data(sqe, wd);
+    struct io_uring_sqe *sqe = io_uring_get_sqe(ring);
+    io_uring_prep_write(sqe, client_fd, hello_response, strlen(hello_response), 0);
+    io_uring_sqe_set_data(sqe, wd);
 }
 
 void add_read_request(struct io_uring *ring, int client_fd)
 {
-	struct read_data *read_data =
-		(struct read_data *)malloc(sizeof(struct read_data));
+    struct read_data *rd = malloc(sizeof(struct read_data));
+    rd->type = OP_READ;
+    rd->fd = client_fd;
 
-	read_data->type = OP_READ;
-	read_data->fd = client_fd;
-
-	struct io_uring_sqe *sqe = io_uring_get_sqe(ring);
-	io_uring_prep_read(sqe, client_fd, read_data->buffer,
-			   sizeof(read_data->buffer), 0);
-	io_uring_sqe_set_data(sqe, read_data);
+    struct io_uring_sqe *sqe = io_uring_get_sqe(ring);
+    io_uring_prep_read(sqe, client_fd, rd->buffer, sizeof(rd->buffer), 0);
+    io_uring_sqe_set_data(sqe, rd);
 }
 
-// Here we are not saving the clients connected in anyway.
 int main(int argc, char **argv)
 {
-	// These data structures are static, we avoid allocating them again and again.
-	struct client_data client_data;
-	struct write_data write_data;
-	struct accept_data accept_data;
-	struct io_uring ring;
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        return 1;
+    }
 
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s <port>\n", argv[0]);
-		return 1;
-	}
+    int server_fd = setup_server_socket_or_die(atoi(argv[1]));
+    struct io_uring ring;
 
-	accept_data.type = OP_ACCEPT;
-	accept_data.server_fd = setup_server_socket_or_die(atoi(argv[1]));
-	client_data.client_addr_len = sizeof(client_data.client_addr);
+    if (io_uring_queue_init(QUEUE_DEPTH, &ring, 0) < 0) {
+        perror("io_uring_queue_init");
+        exit(1);
+    }
 
-	write_data.type = OP_WRITE;
+    add_accept_request(&ring, server_fd);
 
-	if (io_uring_queue_init(QUEUE_DEPTH, &ring, 0) < 0) {
-		perror("io_uring_queue_init");
-		exit(1);
-	}
+    while (1) {
+        io_uring_submit(&ring);
 
-	add_accept_request(&ring, &client_data, &accept_data);
+        struct io_uring_cqe *cqe;
+        if (io_uring_wait_cqe(&ring, &cqe) < 0)
+            continue;
 
-	while (1) {
-		io_uring_submit(&ring);
+        void *data = io_uring_cqe_get_data(cqe);
+        enum op_type type = *(enum op_type *)data;
 
-		struct io_uring_cqe *cqe;
-		if (io_uring_wait_cqe(&ring, &cqe) < 0)
-			continue;
+        switch (type) {
+            case OP_ACCEPT:
+                add_accept_request(&ring, server_fd);
+                if (cqe->res < 0) {
+                    perror("error on accept request");
+                } else {
+                    int client_fd = cqe->res;
+                    add_read_request(&ring, client_fd);
+                }
+                free(data);
+                break;
 
-		if (cqe->res == -ENOBUFS) {
-			fprintf(stderr, "io_ring queue is full");
-		} else {
-			struct read_data *rd;
-			void *data = io_uring_cqe_get_data(cqe);
-			enum op_type type = *(enum op_type *)data;
+            case OP_READ: {
+                struct read_data *rd = data;
+                if (cqe->res > 0) {
+                    add_write_request(&ring, rd->fd);
+                } else {
+                    close(rd->fd);
+                }
+                free(rd);
+                break;
+            }
 
-			switch (type) {
-			case OP_ACCEPT:
-				if (cqe->res < 0) {
-					perror("error on accept request");
-					break;
-				}
-				int client_fd = cqe->res;
-				add_accept_request(&ring, &client_data, data);
-				add_read_request(&ring, client_fd);
-				break;
-			case OP_READ:
-				rd = data;
-				if (cqe->res > 0) {
-					write_data.fd = rd->fd;
-					add_write_request(&ring, &write_data);
-					// Free the read_data after use
-					free(rd);
-					break;
-				}
-				close(rd->fd);
-				free(rd);
-				break;
-			case OP_WRITE:
-				if (cqe->res < 0)
-					perror("failed to write\n");
-				break;
-			default:
-				perror("illegal op on the ring");
-				exit(1);
-			};
-		}
-		io_uring_cqe_seen(&ring, cqe);
-	}
+            case OP_WRITE: {
+                struct write_data *wd = data;
+                if (cqe->res < 0) {
+                    perror("failed to write");
+                }
+                close(wd->fd);
+                free(wd);
+                break;
+            }
 
-	io_uring_queue_exit(&ring);
-	close(accept_data.server_fd);
-	return 0;
+            default:
+                fprintf(stderr, "Unexpected operation\n");
+                exit(1);
+        }
+
+        io_uring_cqe_seen(&ring, cqe);
+    }
+
+    io_uring_queue_exit(&ring);
+    close(server_fd);
+
+    return 0;
 }
